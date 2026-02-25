65ca3177687e33364e3de0db8cfe5e01e25f9f35
Hinoko <johameliasid@gmail.com>
2026-02-25 01:25:02 -0500

    se agrego el archivo de ui llamado index

---
+++ src/renderer/components/ui/index.js
@@ -0,0 +1,10 @@
+//   import { Button, Select, Input, Badge, QuantityInput } from "@/components/ui"
+
+export { default as Button } from "./Button";
+export { default as Select } from "./Select";
+export { default as Input } from "./Input";
+export { default as Badge } from "./Badge";
+export { default as QuantityInput } from "./QuantityInput";
+
+export { ButtonVariant, ButtonSize } from "./Button";
+export { BadgeVariant } from "./Badge";
